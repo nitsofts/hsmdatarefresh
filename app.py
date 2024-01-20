a5996e424ee447dcfabf913bd2bcc0285398afe1
Nitsofts Inc <100211678+nitsofts@users.noreply.github.com>
2024-01-20 18:47:58 +1100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ from flask import Flask
 import os
 import json
 import time
-from datetime import datetime
 
 # Set up basic logging
 logging.basicConfig(level=logging.INFO)
@@ -38,9 +37,6 @@ def update_github_file(message, current_time_ms):
         'Authorization': f'token {GITHUB_TOKEN}',
         'Accept': 'application/vnd.github.v3+json'
     }
-def format_time(milliseconds):
-    timestamp = datetime.fromtimestamp(milliseconds / 1000)
-    return timestamp.strftime('%Y %b %d, %I:%M %p')
 
     # Prepare the content to be updated
     content = json.dumps([{
@@ -77,7 +73,6 @@ def format_time(milliseconds):
 def update_file():
     current_data = fetch_current_data()
     current_time_ms = int(round(time.time() * 1000))
-    current_time_str = format_time(current_time_ms)
 
     if current_data:
         last_refresh_in_ms = current_data[0].get("lastRefreshInMs", 0)
@@ -90,7 +85,6 @@ def update_file():
 
     response_data = {
         "lastRefreshInMs": current_time_ms,
-        "lastRefreshInString": current_time_str,
         "lastRefreshMessage": message
     }
     return response_data, 200 if success else 500
